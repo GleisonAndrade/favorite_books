75d70f04550ed7208dcfacf3540d1eb131a1cede
Gleison Andrade <gleison@infoway-pi.com.br>
2022-01-15 17:07:06 -0300

    fix user_book inflections and validation

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -6,7 +6,7 @@ class UserBook < ApplicationRecord
 
   enumerize :status, in: [:added, :read, :finished, :excluded], scope: true, predicates: true, default: :added
 
-  validates :user, :book, absence: true
+  validates :user_id, :book_id, presence: true
   validates :pages_read, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :favorite, exclusion: [nil]
 end
